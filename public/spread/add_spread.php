995a3554a8c963351e3b33ea36fb2850822743b6
evarisk-kilyan <kilyan.evarisk@gmail.com>
2025-09-12 18:09:02 +0200

    #410 [AddSpread] fix: use $objectMetadata

--- public/spread/add_spread.php
+++ public/spread/add_spread.php
@@ -263,13 +263,15 @@ $ecmFiles->fetchAll('', '', 0, 0, 't.share:isnot:null');
 
 $linkedFiles = [];
 if (is_array($ecmFiles->lines) && !empty($ecmFiles->lines)) {
-    $linkedFiles = array_filter($ecmFiles->lines, function ($ecmFilesLine) use ($objectType, $id) {
+    $linkedFiles = array_filter($ecmFiles->lines, function ($ecmFilesLine) use ($objectType, $id, $objectsMetadata) {
 
         if ($objectType == 'project') {
             $objectType = 'projet';
         } elseif ($objectType == 'project_task') {
             $objectType = 'projet_task';
         }
+
+        $objectType = $objectsMetadata[$objectType]['table_element'];
 
         return $ecmFilesLine->src_object_type == $objectType && $ecmFilesLine->src_object_id == $id;
     });
