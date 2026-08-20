1398e05341bf2b06aec743ada9a3d62f9b39e196
Laurent Magnin <laurent1magnin@gmail.com>
2026-08-20 23:42:52 +0200

    revert: do not hide spread brand on success screen

--- core/tpl/spread/public_spread_view.tpl.php
+++ core/tpl/spread/public_spread_view.tpl.php
@@ -791,7 +791,6 @@ $isSignedPreventionPlan = (!empty($isPreventionPlan) && !empty($signSignatory) &
 
     <div class="public-card__header">
         <div class="public-card__content">
-            <?php if (!($isSignedPreventionPlan && empty(GETPOST('hide_success')))) { ?>
             <div class="spread-brand">
                 <?php if (dol_strlen($spreadLogoUrl)) { ?>
                 <img class="spread-brand__logo" src="<?php echo $spreadLogoUrl; ?>" alt="<?php echo dol_escape_htmltag($mysoc->name); ?>">
@@ -832,7 +831,6 @@ $isSignedPreventionPlan = (!empty($isPreventionPlan) && !empty($signSignatory) &
                 echo ($isLogged ? $objectsMetadata[$objectType]['object']->getNomUrl(1) : dol_escape_htmltag($objectRef)) . (!empty($objectLabel) ? ' - ' . dol_escape_htmltag($objectLabel) : '');
                 ?>
             </div>
-            <?php } ?>
 
 
 
